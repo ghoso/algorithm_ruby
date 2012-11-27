2bd871537ca64d634267e8749e136b13c33c5b0c
Go Hosohara <go.hosohara@gmail.com>
2012-11-27 18:17:20 +0900

    appendメソッド修正。 prependメソッド作成。

--- list.rb
+++ list.rb
@@ -20,16 +20,28 @@ class MyListEntry
     @data = data_info
   end
 
-  def append(new_list)
+  # Append after new_list entry
+  def append(new_entry)
     tmp_entry = nil
-    tmp_entry = self.next_list if self.next_list
-    self.next_list = new_list
+    tmp_entry = self.next_entry if self.next_entry
+    self.next_entry = new_entry
     new_list.prev_entry = self
-    new_list.next_entry = tmp_entry if tmp_entry
+    if tmp_entry
+      new_list.next_entry = tmp_entry
+      self.next_entry.prev_entry = new_entry
+    end
   end
 
-  def prepend(new_list)
-
+  # Prepend before new_list entry
+  def prepend(new_entry)
+    tmp_entry = nil
+    tmp_entry = self.prev_entry if self.prev_entry
+    self.prev_list = new_entry
+    new_entry.next_entry = self
+    if tmp_entry
+      new_entry.prev_entry = tmp_entry
+      self.prev_entry.next_entry = new_entry      
+    end
   end
 
   def delete
